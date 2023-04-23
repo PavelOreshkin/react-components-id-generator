452a2193018ee08af0bda829f7291165167c3702
Pavel Oreshkin <pavel.oreshkin.1@gmail.com>
2023-04-23 22:13:23 +0400

    next

--- index.js
+++ index.js
@@ -1,73 +1,91 @@
 const fs = require('fs');
 const path = require('path');
 const { parseJSX } = require('./acorn');
+const { babelParseJSX } = require('./babel');
 const traverse = require('traverse');
 const astring = require('astring');
 const generator = require('@babel/generator').default;
 
-function addIdsToElement(node, componentName) {
-  const elementType = node.openingElement.name.name;
+// function addIdsToElement(node, componentName) {
+//   const elementType = node.openingElement.name.name;
 
-  if (elementType === 'div') {
-    return;
-  }
+//   if (elementType === 'div') {
+//     return;
+//   }
 
-  const elementId = `${componentName}_${elementType}`;
-  const idAttribute = node.openingElement.attributes.find((attr) => attr.name.name === 'id');
+//   const elementId = `${componentName}_${elementType}`;
+//   const idAttribute = node.openingElement.attributes.find((attr) => attr.name.name === 'id');
 
-  if (idAttribute) {
-    idAttribute.value.value = elementId;
-  } else {
-    node.openingElement.attributes.push({
+//   if (idAttribute) {
+//     idAttribute.value.value = elementId;
+//   } else {
+//     node.openingElement.attributes.push({
+//       type: 'JSXAttribute',
+//       name: {
+//         type: 'JSXIdentifier',
+//         name: 'id',
+//       },
+//       value: {
+//         type: 'Literal',
+//         value: elementId,
+//       },
+//     });
+//   }
+// }
+
+// function addIdsToComponent(componentNode) {
+//   const componentName = componentNode.id.name;
+//   const elements = [];
+
+//   function traverse(node) {
+//     if (node.type === 'JSXElement') {
+//       addIdsToElement(node, componentName);
+//       elements.push(node);
+//     }
+//     node.children.forEach(traverse);
+//   }
+
+//   traverse(componentNode);
+
+//   return elements;
+// }
+
+// function addIdsToFile1(filePath) {
+//   const code = fs.readFileSync(filePath, { encoding: 'utf-8' });
+//   const ast = parseJSX(code);
+//   const components = ast.body.filter((node) => node.type === 'FunctionDeclaration');
+
+//   components.forEach((componentNode) => {
+//     const elements = addIdsToComponent(componentNode);
+//     const newCode = elements.reduce((acc, element) => {
+//       const start = element.start;
+//       const end = element.end;
+//       const elementCode = code.substring(start, end);
+//       const newElementCode = elementCode.replace('<', `<${componentNode.id.name}_`);
+//       return acc.replace(elementCode, newElementCode);
+//     }, code);
+
+//     fs.writeFileSync(filePath, newCode, { encoding: 'utf-8' });
+//   });
+
+//   console.log(`Added ids to ${components.length} components in ${filePath}`);
+// }
+
+function addIdToButton(node) {
+  if (node.type === 'JSXOpeningElement' && node.name.name === 'button') {
+    const idAttribute = {
       type: 'JSXAttribute',
       name: {
         type: 'JSXIdentifier',
         name: 'id',
       },
       value: {
-        type: 'Literal',
-        value: elementId,
+        type: 'StringLiteral',
+        value: '111',
       },
-    });
+    };
+    node.attributes.push(idAttribute);
   }
-}
-
-function addIdsToComponent(componentNode) {
-  const componentName = componentNode.id.name;
-  const elements = [];
-
-  function traverse(node) {
-    if (node.type === 'JSXElement') {
-      addIdsToElement(node, componentName);
-      elements.push(node);
-    }
-    node.children.forEach(traverse);
-  }
-
-  traverse(componentNode);
-
-  return elements;
-}
-
-function addIdsToFile1(filePath) {
-  const code = fs.readFileSync(filePath, { encoding: 'utf-8' });
-  const ast = parseJSX(code);
-  const components = ast.body.filter((node) => node.type === 'FunctionDeclaration');
-
-  components.forEach((componentNode) => {
-    const elements = addIdsToComponent(componentNode);
-    const newCode = elements.reduce((acc, element) => {
-      const start = element.start;
-      const end = element.end;
-      const elementCode = code.substring(start, end);
-      const newElementCode = elementCode.replace('<', `<${componentNode.id.name}_`);
-      return acc.replace(elementCode, newElementCode);
-    }, code);
-
-    fs.writeFileSync(filePath, newCode, { encoding: 'utf-8' });
-  });
-
-  console.log(`Added ids to ${components.length} components in ${filePath}`);
 }
 
 function addIdsToFile(filePath) {
@@ -78,6 +96,9 @@ function addIdsToFile(filePath) {
   // Parse code into AST
   const ast = parseJSX(code);
   console.log('> ast 1: ', ast);
+
+  const astBabel = babelParseJSX(code);
+  console.log('astBabel: ', astBabel);
 
   // Traverse AST and modify the button element
   traverse(ast, {
